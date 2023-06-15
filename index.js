e656464e6b211983242a77f715143c08b87e3d29
misash <misash@outlook.com>
2023-06-15 17:46:48 -0500

    kafka consumer, producer

---
+++ index.js
@@ -0,0 +1,26 @@
+const kafka = require("kafka-node");
+
+const client = new kafka.KafkaClient({
+  kafkaHost: "127.0.0:9092",
+});
+
+/* Consumer*/
+
+consumer = new kafka.Consumer(client, [{ topic: "test" }]);
+
+consumer.on("message", function (message) {
+  console.log(message);
+});
+
+/* Producer */
+
+let producer = new kafka.Producer(client);
+
+producer.on("ready", function () {
+  setInterval(function () {
+    producer.send(
+      [{ topic: "test", messages: "Mensaje automático cada 5 seg." }],
+      function (err, data) {}
+    );
+  }, 5000);
+});
